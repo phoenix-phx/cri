d5435ef287e0de110acd46494ebf554764aa1341
admin <admin@admins-MacBook-Pro.local>
2020-12-16 07:07:47 -0400

    listo vista busquedas, agregar los resultados de la busqueda

---
+++ buscar_investigacion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <h1>Buscar Investigaciones</h1>
+    <h3><i>Elija el filtro e ingrese la busqueda</i></h3>
+    <form>
+        Filtrar por: <select name="filtroBI" id="filtroBI">
+            <option value="Unidad de Investigacion">Unidad de Investigacion</option>
+            <option value="Nombre Investigacion">Nombre Investigacion</option>
+            <option value="Codigo Investigacion">Codigo Investigacion</option>
+            <option value="Nombre Investigador">Nombre Investigador</option>
+        </select>
+        <input name="txtFiltroBI" id="txtFiltroBI" type="text" placeholder="buscar"><br>
+        <input type="submit" value="buscar">
+    </form>
+    <i>Resultados:</i>
+</body>
+</html>
---
+++ buscar_publicacion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <h1>Buscar Publicaciones</h1>
+    <h3><i>Elija el filtro e ingrese la busqueda</i></h3>
+    <form>
+        Filtrar por: <select name="filtroBP" id="filtroBP">
+            <option value="Unidad de Investigacion">Unidad de Investigacion</option>
+            <option value="Nombre">Nombre</option>
+            <option value="Codigo">Codigo</option>
+            <option value="Tipo">Tipo</option>
+        </select>
+        <input name="txtFiltroBP" id="txtFiltroBP" type="text" placeholder="buscar"><br>
+        <input type="submit" value="buscar">
+    </form>
+    <i>Resultados:</i>
+</body>
+</html>
