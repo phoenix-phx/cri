f7184dfafd8e82050367fb20d714bb498ffa3fb3
phoenix-phx <alexander.sosa.asillanes@gmail.com>
2020-12-17 11:54:11 -0400

    actualizado controlador de listas inv - pub para roles diferentes

--- c_listainv.php
+++ c_listainv.php
@@ -7,31 +7,59 @@ if( !isset($_SESSION['idUsuario']) || !isset($_SESSION['permisos'])){
     die('No ha iniciado sesion');
 }
 */
-$sql = 'SELECT codigo, nombre_corto, fecha_fin, idInv 
-    	FROM investigacion
-		WHERE idUsuario = :id'; 
-$stmt = $pdo->prepare($sql);
-$stmt->execute(array(
-   ':id' => $_SESSION['idUsuario']
-));
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-if($row !== false){
-    do{
-        echo '<div role="table">' . "\n";
-        echo '<div role="cabecera"> <span>Codigo</span> </div>';
-        echo '<div role="cabecera"> <span>Nombre Corto</span> </div>';
-        echo '<div role="cabecera"> <span>Fecha Finalizacion</span> </div>';
-        
-        echo '<div role="fila">';
-        echo '<div role="celda"> <span>' . htmlentities($row['codigo']) . '</span> </div>';
-        echo '<div role="celda"> <span>' . htmlentities($row['nombre_corto']) . '</span> </div>';
-        echo '<div role="celda"> <span>' . htmlentities($row['fecha_fin']) . '</span> </div>';
-        echo '<a href="detalles_investigacion_inv.php?inv_id='.$row['idInv'].'">&gt&gt</a>'; echo "</td>";
-        echo "</div>\n";
+if($_SESSION['permisos'] === 'investigador'){
+    $sql = 'SELECT codigo, nombre_corto, fecha_fin, idInv 
+        	FROM investigacion
+    		WHERE idUsuario = :id'; 
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+       ':id' => $_SESSION['idUsuario']
+    ));
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($row !== false){
+        do{
+            echo '<div role="table">' . "\n";
+            echo '<div role="cabecera"> <span>Codigo</span> </div>';
+            echo '<div role="cabecera"> <span>Nombre Corto</span> </div>';
+            echo '<div role="cabecera"> <span>Fecha Finalizacion</span> </div>';
+            
+            echo '<div role="fila">';
+            echo '<div role="celda"> <span>' . htmlentities($row['codigo']) . '</span> </div>';
+            echo '<div role="celda"> <span>' . htmlentities($row['nombre_corto']) . '</span> </div>';
+            echo '<div role="celda"> <span>' . htmlentities($row['fecha_fin']) . '</span> </div>';
+            echo '<a href="detalles_investigacion_inv.php?inv_id='.$row['idInv'].'">&gt&gt</a>'; echo "</td>";
+            echo "</div>\n";
 
-        echo "</div>";
-        echo "<br /> <br />";
-    }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
+            echo "</div>";
+            echo "<br /> <br />";
+        }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
+    }
+    echo "<br />";
 }
-echo "<br />";
+else if($_SESSION['permisos'] === 'administrativo'){
+    $sql = 'SELECT codigo, nombre_corto, unidad_investigacion, idInv 
+            FROM investigacion';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($row !== false){
+        do{
+            echo '<div role="table">' . "\n";
+            echo '<div role="cabecera"> <span>Codigo</span> </div>';
+            echo '<div role="cabecera"> <span>Nombre Corto</span> </div>';
+            echo '<div role="cabecera"> <span>Unidad de Investigacion</span> </div>';
+            
+            echo '<div role="fila">';
+            echo '<div role="celda"> <span>' . htmlentities($row['codigo']) . '</span> </div>';
+            echo '<div role="celda"> <span>' . htmlentities($row['nombre_corto']) . '</span> </div>';
+            echo '<div role="celda"> <span>' . htmlentities($row['unidad_investigacion']) . '</span> </div>';
+            echo '<a href="detalles_investigacion_admin.php?inv_id='.$row['idInv'].'">&gt&gt</a>'; echo "</td>";
+            echo "</div>\n";
+
+            echo "</div>";
+            echo "<br /> <br />";
+        }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
+    }
+    echo "<br />";   
+}
 ?>
--- c_listapub.php
+++ c_listapub.php
@@ -7,31 +7,59 @@ if( !isset($_SESSION['idUsuario']) || !isset($_SESSION['permisos'])){
     die('No ha iniciado sesion');
 }
 */
-$sql = 'SELECT codigo, titulo, tipo, idPub 
-    	FROM publicacion
-		WHERE idUsuario = :id'; 
-$stmt = $pdo->prepare($sql);
-$stmt->execute(array(
-   ':id' => $_SESSION['idUsuario']
-));
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-if($row !== false){
-    do{
-        echo '<div role="table">' . "\n";
-        echo '<div role="cabecera"> <span>Codigo</span> </div>';
-        echo '<div role="cabecera"> <span>Titulo</span> </div>';
-        echo '<div role="cabecera"> <span>Tipo</span> </div>';
-        
-        echo '<div role="fila">';
-        echo '<div role="celda"> <span>' . htmlentities($row['codigo']) . '</span> </div>';
-        echo '<div role="celda"> <span>' . htmlentities($row['titulo']) . '</span> </div>';
-        echo '<div role="celda"> <span>' . htmlentities($row['tipo']) . '</span> </div>';
-        echo '<a href="detalles_publicacion_inv.php?pub_id='.$row['idPub'].'">&gt&gt</a>'; echo "</td>";
-        echo "</div>\n";
-     
-        echo "</div>";
-        echo "<br /> <br />";
-    }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
+if($_SESSION['permisos'] === 'investigador'){
+    $sql = 'SELECT codigo, titulo, tipo, idPub 
+        	FROM publicacion
+    		WHERE idUsuario = :id'; 
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+       ':id' => $_SESSION['idUsuario']
+    ));
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($row !== false){
+        do{
+            echo '<div role="table">' . "\n";
+            echo '<div role="cabecera"> <span>Codigo</span> </div>';
+            echo '<div role="cabecera"> <span>Titulo</span> </div>';
+            echo '<div role="cabecera"> <span>Tipo</span> </div>';
+            
+            echo '<div role="fila">';
+            echo '<div role="celda"> <span>' . htmlentities($row['codigo']) . '</span> </div>';
+            echo '<div role="celda"> <span>' . htmlentities($row['titulo']) . '</span> </div>';
+            echo '<div role="celda"> <span>' . htmlentities($row['tipo']) . '</span> </div>';
+            echo '<a href="detalles_publicacion_inv.php?pub_id='.$row['idPub'].'">&gt&gt</a>'; echo "</td>";
+            echo "</div>\n";
+         
+            echo "</div>";
+            echo "<br /> <br />";
+        }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
+    }
+    echo "<br />";
 }
-echo "<br />";
+else if($_SESSION['permisos'] === 'administrativo'){
+    $sql = 'SELECT codigo, titulo, tipo, idPub 
+            FROM publicacion';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($row !== false){
+        do{
+            echo '<div role="table">' . "\n";
+            echo '<div role="cabecera"> <span>Codigo</span> </div>';
+            echo '<div role="cabecera"> <span>Titulo</span> </div>';
+            echo '<div role="cabecera"> <span>Tipo</span> </div>';
+            
+            echo '<div role="fila">';
+            echo '<div role="celda"> <span>' . htmlentities($row['codigo']) . '</span> </div>';
+            echo '<div role="celda"> <span>' . htmlentities($row['titulo']) . '</span> </div>';
+            echo '<div role="celda"> <span>' . htmlentities($row['tipo']) . '</span> </div>';
+            echo '<a href="detalles_publicacion_inv.php?pub_id='.$row['idPub'].'">&gt&gt</a>'; echo "</td>";
+            echo "</div>\n";
+         
+            echo "</div>";
+            echo "<br /> <br />";
+        }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
+    }
+    echo "<br />";   
+}
 ?>
