ef19e4574484728b522dea1ecbeaa350ebc82837
phoenix-phx <alexander.sosa.asillanes@gmail.com>
2020-12-17 09:58:25 -0400

    Completado controlador de home investigador

--- c_listainv.php
+++ c_listainv.php
@@ -12,23 +12,25 @@ $sql = 'SELECT codigo, nombre_corto, fecha_fin, idInv
 		WHERE idUsuario = :id'; 
 $stmt = $pdo->prepare($sql);
 $stmt->execute(array(
-   ':id' => $_SESSION['idUsuario'],
+   ':id' => $_SESSION['idUsuario']
 ));
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo '<div role="table">' . "\n";
-    echo '<div role="cabecera"> <span>Codigo</span> </div>';
-    echo '<div role="cabecera"> <span>Nombre Corto</span> </div>';
-    echo '<div role="cabecera"> <span>Fecha Finalizacion</span> </div>';
 if($row !== false){
     do{
+        echo '<div role="table">' . "\n";
+        echo '<div role="cabecera"> <span>Codigo</span> </div>';
+        echo '<div role="cabecera"> <span>Nombre Corto</span> </div>';
+        echo '<div role="cabecera"> <span>Fecha Finalizacion</span> </div>';
+        
         echo '<div role="fila">';
         echo '<div role="celda"> <span>' . htmlentities($row['codigo']) . '</span> </div>';
         echo '<div role="celda"> <span>' . htmlentities($row['nombre_corto']) . '</span> </div>';
         echo '<div role="celda"> <span>' . htmlentities($row['fecha_fin']) . '</span> </div>';
         echo '<a href="ver_investigacion.php?inv_id='.$row['idInv'].'">&gt&gt</a>'; echo "</td>";
         echo "</div>\n";
+
+        echo "</div>";
     }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
-    echo "</div>";
 }
 echo "<br />";
 ?>
