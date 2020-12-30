b0a5fca5768197f8dbb377403805811f5473f2d1
phoenix-phx <alexander.sosa.asillanes@gmail.com>
2020-12-30 16:00:04 -0400

    completado lista investigaciones inv/admin OO

--- Investigacion.php
+++ Investigacion.php
@@ -43,6 +43,22 @@ class Investigacion{
 		return $this->resumen;
 	}
 
+	public function setFechaFinal($ff){
+		$this->fecha_fin = $ff;
+	}
+
+	public function getFechaFinal(){
+		return $this->fecha_fin;
+	}
+
+	public function setUnidadInvestigacion($ui){
+		$this->unidad_investigacion = $ui;
+	}
+
+	public function getUnidadInvestigacion(){
+		return $this->unidad_investigacion;
+	}
+
 	public function initInv($user_id, $pdo){
 		$sql = 'SELECT codigo, nombre_corto, resumen, idInv 
 		    	FROM investigacion
@@ -70,6 +86,89 @@ class Investigacion{
 	                echo 'CODIGO: ' . htmlentities($this->getCodigo()) . "<br><br>"."\n";
 	                echo htmlentities($this->getResumen()) . "<br><br>"."\n";
 	            echo '</div>' . "\n";
+	        }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
+	        echo '</div>';
+			return true;
+		}
+	}
+
+	public function listaInv($user_id, $estado, $pdo){
+		$sql = 'SELECT codigo, nombre_corto, fecha_fin, idInv 
+	            FROM investigacion
+	            WHERE idUsuario = :id
+	            AND estado = :st';  // que pasara con las investigaciones terminadas??
+	    $stmt = $pdo->prepare($sql);
+	    $stmt->execute(array(
+	       ':id' => $user_id,
+	       ':st' => $estado
+	    ));
+	    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+	    if($row === false){
+			return false;
+		}
+		else{
+			echo '<div style="padding-left:5%;padding-right:5%;">' . "\n";
+            echo '<div role="cabecera" align="center"> 
+                <div class="aLeft" style="width:320px;">CODIGO</div> 
+                <div class="aLeft" style="width:500px;">NOMBRE CORTO</div> 
+                <div class="aLeft" style="width:250px;">FECHA FINALIZACION</div>
+                </div><br><br>
+            </div>';
+    
+			echo '<div style="padding-left:4%;padding-right:4%;">';
+        	do{
+	        	$this->setCodigo($row['codigo']);
+				$this->setNombreCorto($row['nombre_corto']);
+				$this->setFechaFinal($row['fecha_fin']);
+				$this->setId($row['idInv']);
+
+	            echo '<div role="fila" class="container" 
+	            style="height:60px;padding:10px;padding-top:35px;font-size:18px;" align="center"> 
+	                <div class="aLeft" style="width:320px;">' . htmlentities($this->getCodigo()) . '</div> 
+	                <div class="aLeft" style="width:500px;">' . htmlentities($this->getNombreCorto()) . '</div> 
+	                <div class="aLeft" style="width:250px;">' . htmlentities($this->getFechaFinal()) . '</div>
+	                <a class="link" href="detalles_investigacion_inv.php?inv_id='.$this->getId().'">&gt&gt</a>
+	                </div>';
+	            echo "<br> <br>";
+	        }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
+	        echo '</div>';
+			return true;
+		}
+	}
+
+	public function listaAdmin($pdo){
+		$sql = 'SELECT codigo, nombre_corto, unidad_investigacion, idInv 
+	            FROM investigacion';
+	    $stmt = $pdo->prepare($sql);
+	    $stmt->execute();
+	    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+	    if($row === false){
+			return false;
+		}
+		else{
+			echo '<div style="padding-left:5%;padding-right:5%;">' . "\n";
+			echo '<div role="cabecera" align="center"> 
+			    <div class="aLeft" style="width:320px;">CODIGO</div> 
+			    <div class="aLeft" style="width:500px;">NOMBRE CORTO</div> 
+			    <div class="aLeft" style="width:250px;">UNIDAD DE INVESTIGACION</div>
+			    </div><br><br>
+			</div>';
+
+			echo '<div style="padding-left:4%;padding-right:4%;">';
+        	do{
+	        	$this->setCodigo($row['codigo']);
+				$this->setNombreCorto($row['nombre_corto']);
+				$this->setUnidadInvestigacion($row['unidad_investigacion']);
+				$this->setId($row['idInv']);
+
+	            echo '<div role="fila" class="container" 
+	            style="height:60px;padding:10px;padding-top:35px;font-size:18px;" align="center"> 
+	                <div class="aLeft" style="width:320px;">' . htmlentities($row['codigo']) . '</div> 
+	                <div class="aLeft" style="width:500px;">' . htmlentities($row['nombre_corto']) . '</div> 
+	                <div class="aLeft" style="width:250px;">' . htmlentities($row['unidad_investigacion']) . '</div>
+	                <a class="link" href="detalles_investigacion_admin.php?inv_id='.$row['idInv'].'">&gt&gt</a>
+	                </div>';
+	            echo "<br> <br>";
 	        }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
 	        echo '</div>';
 			return true;
--- c_listainv.php
+++ c_listainv.php
@@ -1,79 +1,24 @@
 <?php 
 session_start();
 require_once "c_pdo.php";
-
+require_once "Investigacion.php";
 
 if( !isset($_SESSION['idUsuario']) || !isset($_SESSION['permisos'])){
     die('No ha iniciado sesion');
 }
 
 if($_SESSION['permisos'] === 'investigador'){
-    $sql = 'SELECT codigo, nombre_corto, fecha_fin, idInv 
-        	FROM investigacion
-    		WHERE idUsuario = :id
-            AND estado = :st';  // que pasara con las investigaciones terminadas??
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(
-       ':id' => $_SESSION['idUsuario'],
-       ':st' => 'en curso'
-    ));
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo '<div style="padding-left:5%;padding-right:5%;">' . "\n";
-            echo '<div role="cabecera" align="center"> 
-                <div class="aLeft" style="width:320px;">CODIGO</div> 
-                <div class="aLeft" style="width:500px;">NOMBRE CORTO</div> 
-                <div class="aLeft" style="width:250px;">FECHA FINALIZACION</div>
-                </div><br><br>
-            </div>';
-            
-
-    if($row !== false){
-        echo '<div style="padding-left:4%;padding-right:4%;">';
-        do{ 
-            echo '<div role="fila" class="container" 
-            style="height:60px;padding:10px;padding-top:35px;font-size:18px;" align="center"> 
-                <div class="aLeft" style="width:320px;">' . htmlentities($row['codigo']) . '</div> 
-                <div class="aLeft" style="width:500px;">' . htmlentities($row['nombre_corto']) . '</div> 
-                <div class="aLeft" style="width:250px;">' . htmlentities($row['fecha_fin']) . '</div>
-                <a class="link" href="detalles_investigacion_inv.php?inv_id='.$row['idInv'].'">&gt&gt</a>
-                </div>';
-            echo "<br> <br>";
-        }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
-        echo '</div>';
-    }
-    else if($row === false){
+    $inv = new Investigacion();
+    $estado = $inv->listaInv($_SESSION['idUsuario'], 'en curso', $pdo);
+    if($estado === false){
         echo "<span> No tiene investigaciones registradas </span>";
     }
     echo "<br />";
 }
 else if($_SESSION['permisos'] === 'administrativo'){
-    $sql = 'SELECT codigo, nombre_corto, unidad_investigacion, idInv 
-            FROM investigacion';
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo '<div style="padding-left:5%;padding-right:5%;">' . "\n";
-    echo '<div role="cabecera" align="center"> 
-        <div class="aLeft" style="width:320px;">CODIGO</div> 
-        <div class="aLeft" style="width:500px;">NOMBRE CORTO</div> 
-        <div class="aLeft" style="width:250px;">UNIDAD DE INVESTIGACION</div>
-        </div><br><br>
-    </div>';
-    
-    if($row !== false){
-        echo '<div style="padding-left:4%;padding-right:4%;">';
-        do{
-            echo '<div role="fila" class="container" 
-            style="height:60px;padding:10px;padding-top:35px;font-size:18px;" align="center"> 
-                <div class="aLeft" style="width:320px;">' . htmlentities($row['codigo']) . '</div> 
-                <div class="aLeft" style="width:500px;">' . htmlentities($row['nombre_corto']) . '</div> 
-                <div class="aLeft" style="width:250px;">' . htmlentities($row['unidad_investigacion']) . '</div>
-                <a class="link" href="detalles_investigacion_admin.php?inv_id='.$row['idInv'].'">&gt&gt</a>
-                </div>';
-                echo "<br> <br>";
-        }while($row = $stmt->fetch(PDO::FETCH_ASSOC));
-    }
-    else if($row === false){
+    $inv = new Investigacion();
+    $estado = $inv->listaAdmin($pdo);
+    if($estado === false){
         echo "<span> No tiene investigaciones registradas </span>";
     }
     echo "<br />";   
