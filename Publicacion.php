339c58e3922f1224efb0f126187a42ce3b1d3db5
phoenix-phx <alexander.sosa.asillanes@gmail.com>
2021-01-03 20:44:46 -0400

    completado subir entrega final OO

--- Publicacion.php
+++ Publicacion.php
@@ -388,5 +388,18 @@ class Publicacion{
 	    $numero = $stmt->fetch(PDO::FETCH_ASSOC);
 	    return $numero;
 	}  
+
+	public function subirEntrega($user_id, $pub_id, $doc, $pdo){
+		$sql = "UPDATE publicacion
+                SET  documento_final = :df
+                WHERE idUsuario = :id
+                AND idPub = :pub";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(
+            ':df' => $doc,
+            ':id' => $user_id,
+            ':pub' => $pub_id
+        ));
+	}  
 }
 ?>
